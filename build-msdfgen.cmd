11fdbf2b85e755832440a5b5e88d05148049f0c2
artem pecheny <pecheny@gmail.com>
2022-10-15 22:42:49 +0300

    Update msdfgen.

--- build-msdfgen.cmd
+++ build-msdfgen.cmd
@@ -1,6 +1,7 @@
+rem if build fails check msdfgen folder for cmake caches and other temporary files
 rd /q /s native\msdfgen-bin
 cd native
 mkdir msdfgen-bin
 cd msdfgen-bin
-cmake  -DCMAKE_TOOLCHAIN_FILE=%vcpkg_path%/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release ../msdfgen -A Win32 
+cmake  -DCMAKE_TOOLCHAIN_FILE=%vcpkg_path%/scripts/buildsystems/vcpkg.cmake -DCMAKE_CXX_FLAGS="/MD" -DCMAKE_BUILD_TYPE=Release ../msdfgen -A Win32 
 cmake --build . --config Release
