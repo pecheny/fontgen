13a150bcf0a75b0192451c906f069f9529a5d318
artem pecheny <pecheny@gmail.com>
2022-10-22 14:55:17 +0300

    Autoformatting.

--- src/Main.hx
+++ src/Main.hx
@@ -21,32 +21,34 @@ typedef Ctx = {
 
 class Main {
 	static inline var SIZE:Int = 4096;
-	
-	static var atlasWidth : Int;
-	static var atlasHeight : Int;
-	
+
+	static var atlasWidth:Int;
+	static var atlasHeight:Int;
+
 	public static var verbose:Bool = false;
 	public static var warnings:Bool = true;
 	public static var info:Bool = false;
 	public static var timings:Bool = false;
 	public static var printMissing:Bool = false;
-	
+
 	public static var globalNonprint:Bool = false;
 	public static var globalr8:Bool = false;
 	public static var sharedAtlas:Bool = false;
 
-	inline static function ts() return haxe.Timer.stamp();
-	inline static function timeStr(ts:Float) return Std.string(Math.round(ts * 1000)) + "ms";
+	inline static function ts()
+		return haxe.Timer.stamp();
+
+	inline static function timeStr(ts:Float)
+		return Std.string(Math.round(ts * 1000)) + "ms";
 
 	static function main() {
 		var configs = readInput();
-		
-		
+
 		var start = ts();
 		Msdfgen.initializeFreetype();
 
 		var globRenderers = [];
-		
+
 		inline function prepareSvgGlyphs(config:GenConfig):Ctx {
 			var rend = new SvgRender(config);
 			var glyphs:Array<GlyphInfo> = [];
@@ -59,8 +61,8 @@ class Main {
 				var glyph = rend.reg(code, fileName, pathName);
 				glyphs.push(glyph);
 			}
-			
-			return {renderers:[rend], glyphs:glyphs};
+
+			return {renderers: [rend], glyphs: glyphs};
 		}
 
 		inline function prepareGlyphs(config:GenConfig):Ctx {
@@ -69,44 +71,48 @@ class Main {
 			if (info) {
 				Sys.println("[Info] Rendering mode: " + config.mode);
 				Sys.println("[Info] Font size: " + config.fontSize);
-				if (!rasterMode) Sys.println("[Info] SDF size: " + config.dfSize);
+				if (!rasterMode)
+					Sys.println("[Info] SDF size: " + config.dfSize);
 				Sys.println("[Info] Output format: text .fnt");
 			}
 
 			var stamp = ts();
 			var renderers:Array<Render> = [];
-			for ( inp in config.inputs ) {
-				if (info) Sys.println("[Info] TTF: " + inp);
+			for (inp in config.inputs) {
+				if (info)
+					Sys.println("[Info] TTF: " + inp);
 				renderers.push(new GlyphRender(inp, config));
 			}
 			var ttfParse = ts();
-			if (timings) Sys.println("[Timing] Parsed ttf: " + timeStr(ttfParse - stamp));
+			if (timings)
+				Sys.println("[Timing] Parsed ttf: " + timeStr(ttfParse - stamp));
 
 			var glyphs:Array<GlyphInfo> = fillGlyphs(config, renderers, ttfParse);
-			return {renderers:renderers, glyphs:glyphs};
+			return {renderers: renderers, glyphs: glyphs};
 		}
 
 		inline function createContext(config:GenConfig):Ctx {
-			return if (config.svgInput != null)
-				prepareSvgGlyphs(config);
-			else 
-				prepareGlyphs(config);
+			return if (config.svgInput != null) prepareSvgGlyphs(config); else prepareGlyphs(config);
 		}
 
-		inline function buildAtlas(ctx:Ctx, config:GenConfig){
+		inline function buildAtlas(ctx:Ctx, config:GenConfig) {
 			var charsetProcess = ts();
 			packGlyphs(config.packer, ctx.glyphs, config.spacing.x, config.spacing.y);
 
 			var glyphPacking = ts();
-			if (info) Sys.println('[Info] Atlas size: ${atlasWidth}x${atlasHeight}');
-			if (timings) Sys.println("[Timing] Glyph packing: " + timeStr(glyphPacking - charsetProcess));
+			if (info)
+				Sys.println('[Info] Atlas size: ${atlasWidth}x${atlasHeight}');
+			if (timings)
+				Sys.println("[Timing] Glyph packing: " + timeStr(glyphPacking - charsetProcess));
 
 			var pngPath = getTexturePath(config);
 			renderAtlas(pngPath, ctx.renderers, config);
 
 			var glyphRendering = ts();
-			if (info) Sys.println("[Info] Writing PNG file to " + pngPath);
-			if (timings) Sys.println("[Timing] Glyph rendering: " + timeStr(glyphRendering - glyphPacking));
+			if (info)
+				Sys.println("[Info] Writing PNG file to " + pngPath);
+			if (timings)
+				Sys.println("[Timing] Glyph rendering: " + timeStr(glyphRendering - glyphPacking));
 		}
 
 		if (sharedAtlas) {
@@ -118,7 +124,7 @@ class Main {
 
 			var mergedCtxs:Ctx = {
 				renderers: [],
-				glyphs:[],
+				glyphs: [],
 			}
 			for (ctx in ctxs) {
 				for (r in ctx.renderers)
@@ -146,7 +152,7 @@ class Main {
 				}
 			}
 		}
-		
+
 		Msdfgen.deinitializeFreetype();
 		if (timings && configs.length > 1) {
 			Sys.println("[Timing] Total work time: " + timeStr(ts() - start));
@@ -170,26 +176,26 @@ class Main {
 		Msdfgen.endAtlas(pngPath);
 	}
 
-	static function writeFntFile(config, glyphs:Array<GlyphInfo>, renderer:Render){
+	static function writeFntFile(config, glyphs:Array<GlyphInfo>, renderer:Render) {
 		function addKerning(file, glyphs:Array<GlyphInfo>, renderer:GlyphRender) {
 			final len = glyphs.length;
 			for (i in 0...len) {
-			var left = glyphs[i];
-			var slot = left.renderer.slot;
-				for (j in (i+1)...len) {
+				var left = glyphs[i];
+				var slot = left.renderer.slot;
+				for (j in (i + 1)...len) {
 					var right = glyphs[j];
 					if (right.renderer.slot == slot) {
 						var kern = Msdfgen.getKerning(slot, left.char, right.char);
 						if (kern != 0) {
-							file.kernings.push({ first: left.char, second: right.char, amount: kern });
+							file.kernings.push({first: left.char, second: right.char, amount: kern});
 						}
 						kern = Msdfgen.getKerning(slot, right.char, left.char);
 						if (kern != 0) {
-							file.kernings.push({ first: right.char, second: left.char, amount: kern });
+							file.kernings.push({first: right.char, second: left.char, amount: kern});
 						}
 					}
 				}
-			}	
+			}
 		}
 
 		// TODO: Optimize: Start building file right away.
@@ -201,11 +207,11 @@ class Main {
 			addKerning(file, glyphs, r);
 		} else {
 			file = new FntFile(config, {
-				 fontName:"Svg graphics",
-				 bold:false,
-				 italic:false,
-				 baseLine:config.fontSize,
-				 lineHeight:config.fontSize,
+				fontName: "Svg graphics",
+				bold: false,
+				italic: false,
+				baseLine: config.fontSize,
+				lineHeight: config.fontSize,
 			});
 		}
 
@@ -218,8 +224,8 @@ class Main {
 				id: g.char,
 				x: Std.int(g.rect.x),
 				y: Std.int(g.rect.y),
-				w: g.width ,
-				h: g.height ,
+				w: g.width,
+				h: g.height,
 				xa: g.advance,
 				xo: g.xOffset,
 				yo: (file.base - g.yOffset),
@@ -231,7 +237,6 @@ class Main {
 		if (timings) {
 			Sys.println("[Timing] FNT generation: " + timeStr(ttfGen - glyphRendering));
 		}
-
 	}
 
 	static function packGlyphs(config:PackerConfig, glyphs:Array<GlyphInfo>, extendWidth:Int, extendHeight:Int) {
@@ -244,9 +249,10 @@ class Main {
 			default: heightSort;
 		}
 		glyphs.sort(sortAlgo);
-		if (inverse) glyphs.reverse();
-		
-		var insert:(w:Int, h:Int)->binpacking.Rect;
+		if (inverse)
+			glyphs.reverse();
+
+		var insert:(w:Int, h:Int) -> binpacking.Rect;
 		switch (config.algorithm) {
 			case "guillotine":
 				var p = new GuillotinePacker(config.width, config.height);
@@ -257,34 +263,35 @@ class Main {
 			case "shelf":
 				var p = new ShelfPacker(config.width, config.height, config.useWasteMap == null ? true : config.useWasteMap);
 				var names = ShelfChoiceHeuristic.getConstructors();
-				var heur = if (names.indexOf(config.heuristic) != -1) ShelfChoiceHeuristic.createByName(config.heuristic);
-					else ShelfChoiceHeuristic.BestArea;
+				var heur = if (names.indexOf(config.heuristic) != -1) ShelfChoiceHeuristic.createByName(config.heuristic); else ShelfChoiceHeuristic.BestArea;
 				insert = p.insert.bind(_, _, heur);
 			case "simple-max-rect":
 				var p = new SimplifiedMaxRectsPacker(config.width, config.height);
 				insert = p.insert;
-			case "skyline": 
+			case "skyline":
 				var p = new SkylinePacker(config.width, config.height, true); // Does not expect usage without waste map.
 				var names = LevelChoiceHeuristic.getConstructors();
-				var heur = if (names.indexOf(config.heuristic) != -1) LevelChoiceHeuristic.createByName(config.heuristic);
-					else LevelChoiceHeuristic.MinWasteFit;
+				var heur = if (names.indexOf(config.heuristic) != -1) LevelChoiceHeuristic.createByName(config.heuristic); else
+					LevelChoiceHeuristic.MinWasteFit;
 				insert = p.insert.bind(_, _, heur);
 			default:
 				var p = new MaxRectsPacker(config.width, config.height, false);
 				var names = FreeRectChoiceHeuristic.getConstructors();
-				var heur = if (names.indexOf(config.heuristic) != -1) FreeRectChoiceHeuristic.createByName(config.heuristic);
-					else FreeRectChoiceHeuristic.BestLongSideFit;
+				var heur = if (names.indexOf(config.heuristic) != -1) FreeRectChoiceHeuristic.createByName(config.heuristic); else
+					FreeRectChoiceHeuristic.BestLongSideFit;
 				insert = p.insert.bind(_, _, heur);
 		}
-		
+
 		var xMax = 0;
 		var yMax = 0;
 		for (g in glyphs) {
 			var rect = g.rect = insert(g.width + extendWidth, g.height + extendHeight);
 			var tmp = Std.int(rect.x + rect.width);
-			if (tmp > xMax) xMax = tmp;
+			if (tmp > xMax)
+				xMax = tmp;
 			tmp = Std.int(rect.y + rect.height);
-			if (tmp > yMax) yMax = tmp;
+			if (tmp > yMax)
+				yMax = tmp;
 		}
 		if (config.exact) {
 			atlasWidth = config.width;
@@ -306,21 +313,25 @@ class Main {
 		var skipNonprint:Bool = config.options.indexOf("allownonprint") == -1 && !globalNonprint;
 		var charsets = Charset.parse(config.charset);
 		var countMissing = charsets.indexOf(Charset.EVERYTHING) == -1;
-		for ( cset in Charset.parse(config.charset) ) {
+		for (cset in Charset.parse(config.charset)) {
 			for (char in cset) {
-				if (skipNonprint && Charset.NONPRINTING.contains(char)) continue;
-				if (inserted.indexOf(char) != -1) continue; // Already rendering with another charset.
+				if (skipNonprint && Charset.NONPRINTING.contains(char))
+					continue;
+				if (inserted.indexOf(char) != -1)
+					continue; // Already rendering with another charset.
 				var found = false;
 				for (renderer in renderers) {
 					var glyph = renderer.get(char);
-					if (glyph == null) continue;
+					if (glyph == null)
+						continue;
 					glyphs.push(glyph);
 					renderer.renderGlyphs.push(glyph);
 					found = true;
 					inserted.push(char);
 					break;
 				}
-				if (countMissing && !found) missing.push(char);
+				if (countMissing && !found)
+					missing.push(char);
 			}
 		}
 
@@ -337,11 +348,13 @@ class Main {
 				Sys.print("\n");
 			}
 		}
-		if (info) Sys.println('[Info] Rendering ${inserted.length} glyphs');
-		if (timings) Sys.println("[Timing] Glyph lookup: " + timeStr(charsetProcess - lastStamp));
+		if (info)
+			Sys.println('[Info] Rendering ${inserted.length} glyphs');
+		if (timings)
+			Sys.println("[Timing] Glyph lookup: " + timeStr(charsetProcess - lastStamp));
 		return glyphs;
 	}
-	
+
 	static function toPOT(v:Int):Int {
 		// https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
 		v--;
@@ -352,14 +365,15 @@ class Main {
 		v |= v >> 16;
 		return v + 1;
 	}
-	
+
 	static function readInput():Array<GenConfig> {
 		var args = Sys.args();
-		if (args.length == 0) printHelp();
-		var flags = args.filter( (a) -> a.charCodeAt(0) == '-'.code );
-		args = args.filter( (a) -> a.charCodeAt(0) != '-'.code );
+		if (args.length == 0)
+			printHelp();
+		var flags = args.filter((a) -> a.charCodeAt(0) == '-'.code);
+		args = args.filter((a) -> a.charCodeAt(0) != '-'.code);
 		var stdinConfig:Bool = false;
-		
+
 		for (arg in flags) {
 			switch (arg.toLowerCase()) {
 				case "-verbose":
@@ -401,11 +415,14 @@ class Main {
 		var baseDir = Sys.getCwd();
 		for (arg in args) {
 			if (FileSystem.exists(arg)) {
-				if (Path.isAbsolute(arg)) Sys.setCwd(Path.directory(arg));
+				if (Path.isAbsolute(arg))
+					Sys.setCwd(Path.directory(arg));
 				else {
 					var dir = Path.directory(arg);
-					if (dir == "") Sys.setCwd(baseDir);
-					else Sys.setCwd(Path.join([baseDir, dir]));
+					if (dir == "")
+						Sys.setCwd(baseDir);
+					else
+						Sys.setCwd(Path.join([baseDir, dir]));
 				}
 				return jsonConfig(File.getContent(arg));
 			}
@@ -414,7 +431,7 @@ class Main {
 		printHelp();
 		return null;
 	}
-	
+
 	static function printHelp() {
 		#if (hlc || cpp)
 		final exe = Sys.systemName() == "Windows" ? "fontgen.exe" : "fontgen";
@@ -426,7 +443,7 @@ class Main {
 		Sys.println(StringTools.replace(haxe.Resource.getString("help"), "{{exe}}", exe));
 		Sys.exit(0);
 	}
-	
+
 	static function jsonConfig(inp:String):Array<GenConfig> {
 		var cfg:Dynamic = Json.parse(inp);
 		if (Std.isOfType(cfg, Array)) {
@@ -457,8 +474,8 @@ class Main {
 				}
 			} else {
 				for (cfg in arr) {
-                    if (isNullOrEmpty(cfg.pngName))
-					cfg.pngName = Path.withoutExtension(cfg.output);
+					if (isNullOrEmpty(cfg.pngName))
+						cfg.pngName = Path.withoutExtension(cfg.output);
 				}
 			}
 			return arr;
@@ -467,13 +484,13 @@ class Main {
 		}
 	}
 
-    static function mergeConfig(target:GenConfig, source:GenConfig) {
+	static function mergeConfig(target:GenConfig, source:GenConfig) {
 		for (key in Reflect.fields(source)) {
 			if (!Reflect.hasField(target, key)) {
 				Reflect.setField(target, key, Reflect.field(source, key));
 			}
 		}
-    }
+	}
 
 	static function fillTemplate(cfg:GenConfig) {
 		if (cfg.template == null)
@@ -482,50 +499,70 @@ class Main {
 			Sys.println('[Warn] template ${cfg.template} not found');
 			return;
 		}
-		var template = Json.parse (File.getContent(cfg.template));
-        mergeConfig(cfg, template);
+		var template = Json.parse(File.getContent(cfg.template));
+		mergeConfig(cfg, template);
 	}
-	
+
 	static function fillDefaults(cfg:GenConfig):GenConfig {
-		if ( cfg.mode == null ) cfg.mode = MSDF;
+		if (cfg.mode == null)
+			cfg.mode = MSDF;
 		else {
-			cfg.mode = (cfg.mode:String).toLowerCase();
+			cfg.mode = (cfg.mode : String).toLowerCase();
 			cfg.mode.validate();
 		}
-		if ( cfg.fontSize == null ) throw "Font size should be specified!";
-		if (cfg.svgInput != null ) {
-		} else if ( cfg.inputs == null ) {
-			if (( cfg.input == null || !FileSystem.exists(cfg.input) )
-				&& ( cfg.svgInput == null  )) throw "Path to TTF or SVG file should be specified!";
+		if (cfg.fontSize == null)
+			throw "Font size should be specified!";
+		if (cfg.svgInput != null) {} else if (cfg.inputs == null) {
+			if ((cfg.input == null || !FileSystem.exists(cfg.input)) && (cfg.svgInput == null))
+				throw "Path to TTF or SVG file should be specified!";
 			cfg.inputs = [cfg.input];
 		} else {
-			if ( cfg.input != null ) cfg.inputs.unshift(cfg.input);
-			for ( inp in cfg.inputs ) {
-				if (!FileSystem.exists(inp)) throw "Input file does not exists: " + inp;
+			if (cfg.input != null)
+				cfg.inputs.unshift(cfg.input);
+			for (inp in cfg.inputs) {
+				if (!FileSystem.exists(inp))
+					throw "Input file does not exists: " + inp;
 			}
 		}
-		if ( cfg.options == null ) cfg.options = [];
+		if (cfg.options == null)
+			cfg.options = [];
 		else {
 			for (i in 0...cfg.options.length) {
 				cfg.options[i] = cfg.options[i].toLowerCase();
 			}
 		}
-		if ( cfg.output == null ) throw "Output to FNT file should be specified!";
-		if ( cfg.charset == null || cfg.charset.length == 0 ) cfg.charset = ["everything"];
-		if ( cfg.dfSize == null ) cfg.dfSize = 6;
-		if ( cfg.padding == null ) cfg.padding = { top: 0, bottom: 0, left: 0, right: 0 };
+		if (cfg.output == null)
+			throw "Output to FNT file should be specified!";
+		if (cfg.charset == null || cfg.charset.length == 0)
+			cfg.charset = ["everything"];
+		if (cfg.dfSize == null)
+			cfg.dfSize = 6;
+		if (cfg.padding == null)
+			cfg.padding = {
+				top: 0,
+				bottom: 0,
+				left: 0,
+				right: 0
+			};
 		else {
-			if ( cfg.padding.top == null ) cfg.padding.top = 0;
-			if ( cfg.padding.bottom == null ) cfg.padding.bottom = 0;
-			if ( cfg.padding.left == null ) cfg.padding.left = 0;
-			if ( cfg.padding.right == null ) cfg.padding.right = 0;
+			if (cfg.padding.top == null)
+				cfg.padding.top = 0;
+			if (cfg.padding.bottom == null)
+				cfg.padding.bottom = 0;
+			if (cfg.padding.left == null)
+				cfg.padding.left = 0;
+			if (cfg.padding.right == null)
+				cfg.padding.right = 0;
 		}
-		if ( cfg.spacing == null ) cfg.spacing = { x: 1, y: 1 };
+		if (cfg.spacing == null)
+			cfg.spacing = {x: 1, y: 1};
 		else {
-			if ( cfg.spacing.x == null) cfg.spacing.x = 0;
-			if ( cfg.spacing.y == null) cfg.spacing.y = 0;
+			if (cfg.spacing.x == null)
+				cfg.spacing.x = 0;
+			if (cfg.spacing.y == null)
+				cfg.spacing.y = 0;
 		}
-		
+
 		if (cfg.packer == null) {
 			cfg.packer = {
 				size: null,
@@ -542,42 +579,41 @@ class Main {
 			if (cfg.packer.size != null) {
 				cfg.packer.width = cfg.packer.height = cfg.packer.size;
 			} else {
-				if (cfg.packer.width == null) cfg.packer.width = 4096;
-				if (cfg.packer.height == null) cfg.packer.height = 4096;
-				if (cfg.packer.sort == null) cfg.packer.sort = "-height";
-				else cfg.packer.sort = cfg.packer.sort.toLowerCase();
-				if (cfg.packer.algorithm == null) cfg.packer.algorithm = "max-rect";
-				else cfg.packer.algorithm = cfg.packer.algorithm.toLowerCase();
+				if (cfg.packer.width == null)
+					cfg.packer.width = 4096;
+				if (cfg.packer.height == null)
+					cfg.packer.height = 4096;
+				if (cfg.packer.sort == null)
+					cfg.packer.sort = "-height";
+				else
+					cfg.packer.sort = cfg.packer.sort.toLowerCase();
+				if (cfg.packer.algorithm == null)
+					cfg.packer.algorithm = "max-rect";
+				else
+					cfg.packer.algorithm = cfg.packer.algorithm.toLowerCase();
 			}
 		}
-		
+
 		return cfg;
 	}
 
-
-	
-	static function widthSort(a:GlyphInfo, b:GlyphInfo):Int
-	{
+	static function widthSort(a:GlyphInfo, b:GlyphInfo):Int {
 		return Math.round(a.width - b.width);
 	}
-	
-	static function heightSort(a:GlyphInfo, b:GlyphInfo):Int
-	{
+
+	static function heightSort(a:GlyphInfo, b:GlyphInfo):Int {
 		return Math.round(a.height - b.height);
 	}
-	
-	static function areaSort(a:GlyphInfo, b:GlyphInfo):Int
-	{
+
+	static function areaSort(a:GlyphInfo, b:GlyphInfo):Int {
 		return Math.round((a.width * a.height) - (b.width * b.height));
 	}
-	
-	static function perimeterSort(a:GlyphInfo, b:GlyphInfo):Int
-	{
+
+	static function perimeterSort(a:GlyphInfo, b:GlyphInfo):Int {
 		return Math.round((a.width + a.height) - (b.width + b.height));
 	}
-	
-	static function idSort(a:GlyphInfo, b:GlyphInfo):Int
-	{
+
+	static function idSort(a:GlyphInfo, b:GlyphInfo):Int {
 		return a.char - b.char;
 	}
 
@@ -592,5 +628,4 @@ class Main {
 		var path = Path.directory(cfg.output);
 		return Path.withExtension(Path.join([path, cfg.pngName]), "png");
 	}
-	
 }
