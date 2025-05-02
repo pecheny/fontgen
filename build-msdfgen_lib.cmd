df29fb5ca3690ebbcf2f1bd931b5af0b7b7a2ed2
artem pecheny <pecheny@gmail.com>
2025-05-02 14:49:58 +0300

    Remove outdated configs.

--- build-msdfgen_lib.cmd
+++
@@ -1,6 +0,0 @@
-pushd native
-del msdfgen_lib.obj
-del msdfgen_lib.exp
-del msdfgen_lib.lib
-del msdfgen_lib.dll
-vcvars && nmake Makefile.win
--- clean.cmd
+++
@@ -1,8 +0,0 @@
-rd /s /q bin\hlc
-rd /s /q native\hl
-rd /s /q native\msdfgen-bin
-
-del native\msdfgen_lib.obj
-del native\msdfgen_lib.exp
-del native\msdfgen_lib.lib
-del bin\ammer_msdfgen_lib.hdll 
